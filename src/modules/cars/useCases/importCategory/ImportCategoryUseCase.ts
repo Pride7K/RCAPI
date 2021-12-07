7076810471e430e385eea2b9b37328c98c88587b
Pride7K <gsantos2750@gmail.com>
2021-12-07 12:49:24 -0300

    upload route use case

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,52 @@
+import csvParser from "csv-parser"
+import fs from "fs";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
+
+export class ImportCategoryUseCase {
+
+    constructor(private categoriesRepository: ICategoriesRepository) {
+
+    }
+
+
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+
+            const parseFile = csvParser({
+
+            });
+
+            stream.pipe(parseFile)
+
+            const categories: IImportCategory[] = [];
+
+            parseFile.on("data", async (line) => {
+                var { name, description } = line;
+                categories.push({
+                    name, description
+                })
+            }).on("end", () => resolve(categories))
+              .on("error", (error) => {
+                return reject(error)})
+        });
+    }
+
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategories(file);
+        categories.map(category => {
+            const { name, description } = category
+            const existCategory = this.categoriesRepository.findByName(name);
+            if (!existCategory) {
+                this.categoriesRepository.create({
+                    name, description
+                })
+            }
+        })
+    }
+}
